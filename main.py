d90c75e779ed4e85a2acc472ce6dfa87847789bf
Manuel A. Vázquez <manuavazquez@gmail.com>
2016-08-29 12:21:54 +0200

    - main.py 	- loop over a certain number of trials

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ from sklearn.mixture import GMM
 
 # --------------------- general parameters
 
+# number of trials
+n_trials = 100
+
 # mixture coefficient
 ro = 0.2
 
@@ -23,10 +26,15 @@ M = 10000
 # hyperparameters for the prior pdf
 nu, lamb = 1, 0.1
 
-# number of highest weights for the clipping procedure
-M_T = M//10
+# ratio between the number of particles and that of non-negligible weights
+M_T_over_M = 0.1
 
 # ---------------------
+
+# number of highest weights for the clipping procedure
+M_T = int(M*M_T_over_M)
+
+estimates = np.empty((n_trials, 2, 2))
 
 # the proposal is a Gaussian density with parameters...
 proposal_mean, proposal_sd = nu, np.sqrt(variance/lamb)
@@ -42,55 +50,52 @@ gmm.means_ = np.reshape(true_means, (-1, 1))
 gmm.covars_ = np.full(shape=(2,1),fill_value=variance, dtype=float)
 gmm.weights_ = np.array([ro, 1-ro])
 
-observations = gmm.sample(n_samples=N, random_state=prng).flatten()
+for i_trial in range(n_trials):
 
-# a samples is drawn for every particle *and* every component
-samples = prng.normal(loc=proposal_mean, scale=proposal_sd, size=(M, 2))
+	observations = gmm.sample(n_samples=N, random_state=prng).flatten()
 
-# computation of the factor every *individual* observations contributes to the likelihood
-# (every row is associated with an observation, and every column with a particle)
-likelihood_factors = ro*np.exp(-(observations[:, np.newaxis]-samples[:, 0][np.newaxis, :])**2
-                               /(2*variance))/np.sqrt(2*np.pi*variance) + \
-                     (1.0-ro)*np.exp(-(observations[:, np.newaxis]-samples[:, 1][np.newaxis, :])**2
-                                     /(2*variance))/np.sqrt(2*np.pi*variance)
+	# samples are drawn for every particle *and* every component
+	samples = prng.normal(loc=proposal_mean, scale=proposal_sd, size=(M, 2))
 
-# the likelihood is given by the product of the individual factors
-likelihood = likelihood_factors.prod(axis=0)
+	# computation of the factor every *individual* observations contributes to the likelihood
+	# (every row is associated with an observation, and every column with a particle)
+	likelihood_factors = ro*np.exp(-(observations[:, np.newaxis]-samples[:, 0][np.newaxis, :])**2
+	                               /(2*variance))/np.sqrt(2*np.pi*variance) + \
+	                     (1.0-ro)*np.exp(-(observations[:, np.newaxis]-samples[:, 1][np.newaxis, :])**2
+	                                     /(2*variance))/np.sqrt(2*np.pi*variance)
 
-# weights are obtained by normalizing the values above
-weights = likelihood / likelihood.sum()
+	# the likelihood is given by the product of the individual factors
+	likelihood = likelihood_factors.prod(axis=0)
 
-# regular IW-based estimate
-regular_estimate = weights @ samples
+	# weights are obtained by normalizing the values above
+	weights = likelihood / likelihood.sum()
 
-# MMSE
-regular_mmse = np.sum((regular_estimate - true_means)**2)
+	# regular IW-based estimate
+	estimates[i_trial, :, 0] = weights @ samples
 
-# --------------------- transformed importance weights
+	# --------------------- transformed importance weights
 
-# NOTE: *likelihood* is modified
+	# NOTE: *likelihood* is modified
 
-# clipping
-i_clipped = np.argpartition(likelihood, -M_T)[-M_T:]
+	# clipping
+	i_clipped = np.argpartition(likelihood, -M_T)[-M_T:]
 
-# minimum weight among those to be clipped
-clipping_threshold = likelihood[i_clipped[0]]
+	# minimum weight among those to be clipped
+	clipping_threshold = likelihood[i_clipped[0]]
 
-# the largest weights are "clipped"
-likelihood[i_clipped] = clipping_threshold
+	# the largest weights are "clipped"
+	likelihood[i_clipped] = clipping_threshold
 
-# weights are obtained by normalizing the values above
-weights = likelihood / likelihood.sum()
+	# weights are obtained by normalizing the values above
+	weights = likelihood / likelihood.sum()
 
-#  TIW-based estimate
-tiw_estimate = weights @ samples
+	#  TIW-based estimate
+	estimates[i_trial, :, 1] = weights @ samples
 
-# performance
-tiw_mmse = np.sum((tiw_estimate - true_means)**2)
+# MMSE computation (every row is a trial, every column a component of the state vector)
+mmse = np.sum((estimates - true_means[np.newaxis, :, np.newaxis])**2, axis=1)
 
-# n_largest_weights = 10
-#
-# # the "n_largest_weights" higher weights
-# i_largest_weights = np.argpartition(weights, -n_largest_weights)[-n_largest_weights:]
-# weights[i_largest_weights]
-# print('the {} best particles are\n{}'.format(n_largest_weights, samples[i_largest_weights, :]))
+average_mmse = mmse.mean(axis=0)
+
+# variance (every row is an *algorithm*, every column a component of the state vector)
+variance = np.var(estimates, axis=0).T
